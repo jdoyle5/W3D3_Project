0164c9ebabe36c64f959994ad18014235a217398
Joey Doyle <j.doyle345@gmail.com>
2017-10-18 16:56:11 -0700

    Finish Phase II of URL Shortener project

---
+++ URL_shortener/URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,38 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :integer          not null, primary key
+#  short_url  :string           not null
+#  long_url   :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class ShortenedUrl < ApplicationRecord
+
+  validates :user_id, :short_url, :long_url, presence: true
+  validates :user_id, :short_url, uniqueness: true
+
+  def self.random_code
+    code = SecureRandom.urlsafe_base64
+    while ShortenedUrl.exists?(short_url: code)
+      code = SecureRandom.urlsafe_base64
+    end
+    code
+  end
+
+  # ShortenedUrl::make_shortened_url: User, string => ShortenedUrl instance
+  def self.make_shortened_url(user, long_url)
+    shortened = ShortenedUrl.new(user_id: user.id, long_url: long_url)
+    shortened.short_url = ShortenedUrl.random_code
+    shortened.save
+    shortened
+  end
+
+  belongs_to :submitter,
+  primary_key:  :id,
+  foreign_key:  :user_id,
+  class_name:   :User
+end
---
+++ URL_shortener/URLShortener/app/models/user.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ApplicationRecord
+  has_many :submitted_urls,
+           primary_key: :id,
+           foreign_key: :user_id,
+           class_name: :ShortenedUrl
+end
